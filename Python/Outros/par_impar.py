27f04baacde28ffb314e926ddfd048041eb76c8b
Ranbut <59321138+Ranbut@users.noreply.github.com>
2021-06-08 15:53:40 -0300

    Add files via upload

---
+++ Python/Outros/par_impar.py
@@ -0,0 +1,9 @@
+print("Par ou Impar?")
+while True:
+    print(" ")
+    n = int(input('Digite um inteiro: '))
+    print(" ")
+    if (n % 2) == 0:
+        print(n, ": é um número par!")
+    else:
+        print(n, ": é um número ímpar!")
